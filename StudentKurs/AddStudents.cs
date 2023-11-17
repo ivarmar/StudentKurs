8502f50c2d979079e10673275151fb125d65a05d
ivarmar <141323294+ivarmar@users.noreply.github.com>
2023-11-17 15:11:54 +0100

    Add project files.

---
+++ StudentKurs/AddStudents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentKurs
+{
+    internal class Student
+    {
+        public Student(string name, int age, string course, string chosenCourse)
+        {
+            Name = name;
+            Age = age;
+            Course = course;
+            ChosenCourse = chosenCourse;
+        }
+
+        public string Name { get; set; } 
+        public int Age {  get; set; }
+        public string Course { get; set; }
+
+        public string ChosenCourse { get; set; }
+
+    }
+}
---
+++ StudentKurs/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StudentKurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            static Student MakeStudent()
+            {
+                Console.WriteLine("Student Name:");
+                var newStudentName = Console.ReadLine();
+                Console.WriteLine("Student age:");
+                var newStudentAge = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Student course:");
+                var newStudentCourse = Console.ReadLine();
+                Console.WriteLine($" Welcome {newStudentName}, What course would you like to enroll in? the current options are ");
+                Console.WriteLine($"'1': Get Academy\n'2':SharkDiving.\n'3':Learning to get Tan behind glass.");
+                var newChosenCourse = Console.ReadLine();
+
+
+                return new Student (newStudentName, newStudentAge, newStudentCourse, newChosenCourse);
+                
+            }
+
+            var RegisteredStudent = MakeStudent();
+            Console.WriteLine($"{MakeStudent}");
+        }
+    }
+}
